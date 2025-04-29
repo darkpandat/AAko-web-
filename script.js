53170460a322e909b956c56668f1d91c981e3d68
𓆩𝗣𝗥𝗜𝗡𝗖𝗘𓆪™ 🪬 <130037584+darkpandat@users.noreply.github.com>
2025-04-29 13:21:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-let wheel = document.getElementById("wheel");
-let result = document.getElementById("result");
-let ytLink = document.getElementById("yt-link");
-
 function spin() {
   let rotate = 3600 + Math.floor(Math.random() * 360);
-  wheel.style.transform = rotate(${rotate}deg);
+  wheel.style.transform = `rotate(${rotate}deg)`;
   result.textContent = "Spinning...";
   ytLink.style.display = "none";
 
   setTimeout(() => {
     result.textContent = "Congratulations! You won an iPhone!";
     ytLink.textContent = "Click here to claim your prize";
-    ytLink.href = "https://youtube.com/@justtakko?si=x5Yc-0VnvtOS2-KG"; // Apna YT link daal yahan
+    ytLink.href = "https://youtube.com/@justtakko?si=x5Yc-0VnvtOS2-KG"; // Apna YT link
     ytLink.style.display = "block";
   }, 4000);
 }
